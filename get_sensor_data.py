d1ef9e06a71ec2fc329c1be46770b59bc124485d
Adam <adam.593304@gmail.com>
2019-02-16 18:33:13 +0100

    Update 3 - Code cleanup

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -11,12 +11,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('mac')
     args = parser.parse_args()
-    
+
     # Getting data from Mi Temperature and Humidity Sensor
     poller = MiTempBtPoller(args.mac, BluepyBackend)
-    result = '"battery": {0}, "temperature": {1}, "humidity": {2}'.format(poller.parameter_value(MI_BATTERY), poller.parameter_value(MI_TEMPERATURE), poller.parameter_value(MI_HUMIDITY))
-    
+    result = '"battery": {0}, "temperature": {1}, "humidity": {2}'\
+        .format(poller.parameter_value(MI_BATTERY),
+                poller.parameter_value(MI_TEMPERATURE),
+                poller.parameter_value(MI_HUMIDITY))
+
     print(result)
+
 
 if __name__ == '__main__':
     main()
